461ec31c7bf12a17ac27add1550c075ccb89e162
eeki <ifirtree@gmail.com>
2018-09-10 22:47:49 +0300

    сделана выдача главной страницы

---
+++ server/config.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+    version: '1.0'
+    , server: {
+        port: 3000
+        , host: 'localhost' 
+        //, protocol: 'http' // "https",
+	}
+};
--- server/index.js
+++ server/index.js
@@ -3,24 +3,25 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 
-//const api = require('./routes/api'); // импортируем роутер
+const config = require('./config');
+//const api = require('./api/routes'); // импортируем роуты
 
 const indexHTML = path.resolve(__dirname,'./public/index.html');
 const app = express();
 const port = 3000;
 
-// все статические файлы в папку public
+// статические файлы
 app.use('/', express.static('public'));
 
-// запросы к api выносите в отдельный файл и подключаете как миделвер
+// запросы к api в отдельном файле
 //app.use('/api', api);
 
-// на все остальные запросы 
+// на все остальные запросы отдаем главную страницу
 app.get('/*', (req, res) => res.sendFile(indexHTML));
 
 app.listen(port, (err) => {
     if (err) {
-        return console.log('something bad happened', err)
+        return console.log('error', err);
     }
-    console.log(`server is listening on ${port}`)
+    console.log(`server is listening on ${port}`);
 });
