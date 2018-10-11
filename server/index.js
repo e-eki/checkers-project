0f550104ba9f4b2797777c2d34e3c00505f9fb13
eeki <ifirtree@gmail.com>
2018-10-11 20:54:28 +0300

    0

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ app.use(express.static('front-end/public'));
 app.use((req, res, next) => {
     res.set({
         'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
     });
 
